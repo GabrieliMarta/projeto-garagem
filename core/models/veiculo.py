af00cc06a02d6de6144e8192be3de659c9204077
gabrielimarta <gabrielirocha2730@gmail.com>
2025-05-22 16:21:02 -0300

    fix:  troca de cascade para protect

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -3,8 +3,8 @@ from django.db import models
 class Veiculo(models.Model):
    ano = models.IntegerField(null=True, blank=True, default=0)
    preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=0)
-   modelo = models.ForeignKey('Modelo', on_delete=models.CASCADE)
-   cor = models.ForeignKey('Cor', on_delete=models.CASCADE)
+   modelo = models.ForeignKey('Modelo', on_delete=models.PROTECT)
+   cor = models.ForeignKey('Cor', on_delete=models.PROTECT)
    acessorios = models.ManyToManyField('Acessorio', blank=True)
 
    def __str__(self):
